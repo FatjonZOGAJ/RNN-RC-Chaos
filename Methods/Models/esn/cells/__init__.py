fa72609d27af99e15db240e4a29f65de376c658a
Rafael <rafael.sterzinger@gmx.at>
2021-12-27 20:00:33 +0100

    Resolve esn cell ambiguity

--- Methods/Models/esn/cells/__init__.py
+++ Methods/Models/esn/cells/__init__.py
@@ -1,4 +1,5 @@
-from rc_chaos.Methods.Models.esn.cells.esn_cell import ESNCell
+from rc_chaos.Methods.Models.esn.cells.esn_cell import ESNCell as ESNCell_numpy
+from rc_chaos.Methods.Models.esn.cells.esn_cell_torch import ESNCell as ESNCell_torch
 from rc_chaos.Methods.Models.esn.cells.gru_cell import GRUCell
 from rc_chaos.Methods.Models.esn.cells.lstm_cell import LSTMCell
 from rc_chaos.Methods.Models.esn.cells.rnn_cell import RNNCell
@@ -12,9 +13,9 @@ def get_cell(type, reservoir_size, radius, sparsity, sigma_input):
     elif type == 'RNN':
         return RNNCell(reservoir_size)
     elif type == 'ESN':
-        return ESNCell(reservoir_size, radius, sparsity, sigma_input)
+        return ESNCell_numpy(reservoir_size, radius, sparsity, sigma_input)
     elif type == 'ESN_torch':
-        from cells.esn_cell_torch import ESNCell
-        return ESNCell(reservoir_size, radius, sparsity, sigma_input)
+        from esn_cell_torch import ESNCell
+        return ESNCell_torch(reservoir_size, radius, sparsity, sigma_input)
     else:
         raise RuntimeError('Unknown cell type.')
